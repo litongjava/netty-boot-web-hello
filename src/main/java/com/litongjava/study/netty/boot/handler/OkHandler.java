3b99768b8d5ad265d7f7b66ae576ddc83cc72630
litongjava <litongjava@qq.com>
2025-01-09 18:30:58 -1000

    update netty-boot version

--- src/main/java/com/litongjava/study/netty/boot/handler/OkHandler.java
+++ src/main/java/com/litongjava/study/netty/boot/handler/OkHandler.java
@@ -1,27 +1,27 @@
 package com.litongjava.study.netty.boot.handler;
 
 import com.litongjava.model.body.RespBodyVo;
+import com.litongjava.netty.boot.adapter.HttpRequest;
+import com.litongjava.netty.boot.adapter.HttpResponse;
 import com.litongjava.netty.boot.utils.HttpRequestUtils;
 import com.litongjava.netty.boot.utils.HttpResponseUtils;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
 
 public class OkHandler {
 
-  public FullHttpResponse txt(ChannelHandlerContext ctx, FullHttpRequest httpRequest) throws Exception {
+  public HttpResponse txt(ChannelHandlerContext ctx, HttpRequest httpRequest) throws Exception {
     String responseContent = "Hello, this is the HTTP response!";
     return HttpResponseUtils.txt(responseContent);
   }
 
-  public FullHttpResponse json(ChannelHandlerContext ctx, FullHttpRequest httpRequest) throws Exception {
+  public HttpResponse json(ChannelHandlerContext ctx, HttpRequest httpRequest) throws Exception {
     RespBodyVo ok = RespBodyVo.ok();
     return HttpResponseUtils.json(ok);
   }
 
-  public FullHttpResponse echo(ChannelHandlerContext ctx, FullHttpRequest httpRequest) {
-    String fullHttpRequestString = HttpRequestUtils.getFullHttpRequestAsString(httpRequest);
+  public HttpResponse echo(ChannelHandlerContext ctx, HttpRequest httpRequest) {
+    String fullHttpRequestString = HttpRequestUtils.getHttpRequestAsString(httpRequest);
     return HttpResponseUtils.txt(fullHttpRequestString);
   }
 }
